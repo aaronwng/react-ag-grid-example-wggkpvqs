be373960c01391cd1ef26d068de48698cdaa785a
aaronwng <awong@sumologic.com>
2025-02-24 22:02:42 -0700

    Two versions ag-grid

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppV2 from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <script src="https://cdn.jsdelivr.net/npm/ag-grid-community@31.3.4/dist/ag-grid-community.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/ag-grid-enterprise@31.3.4/dist/ag-grid-enterprise.min.js"></script>
     <App />
+    <AppV2/>
   </React.StrictMode>,
   document.getElementById('root')
 );
